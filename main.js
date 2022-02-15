2225f54a21521d9071bdf294d7cca2af7f0cd75d
rashichakraborty <77398317+rashichakraborty@users.noreply.github.com>
2022-02-15 16:50:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+console.log('ml5 version',ml5.version);
+
+classifier = ml5.imageClassifier('https//teachablemachine.withgoogle.com/models/ufz1CF4ls/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded');
+}
+
+prediction_1 = "";
+prediction_2 = "";
+
+Webcam.set({
+    width:350,
+    height:300,
+    dest_width:450,
+    dest_height:400,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is"+ prediction_1;
+    speak_data_2 = "The second prediction is"+ prediction_2;
+    var utter = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utter);
+}
